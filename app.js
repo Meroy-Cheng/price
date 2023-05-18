ded88a3b1c18c566cbc546e5234855d1e0b5c25c
May <mutsen011128@gmail.com>
2023-05-18 13:40:02 +0800

    fix

--- app.js
+++ app.js
@@ -1,100 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const PORT = 3000;
 
-var app = express();
+// 設置body-parser中間件
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// 假設記錄存儲在一個數組中
+let records = [];
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// 處理GET請求，返回所有記錄
+app.get("/records", (req, res) => {
+  res.json(records);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// 處理POST請求，新增記錄
+app.post("/records", (req, res) => {
+  const { date, productName, price } = req.body;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // 做一些驗證和處理
+
+  const record = {
+    date: date,
+    productName: productName,
+    price: price
+  };
+
+  records.push(record);
+  res.json(record);
 });
 
-module.exports = app;
-// app.js
-//----------------------------------------------------------------------------------------------------------------
+// 啟動服務器
 
-// 假設價格資料存儲在一個陣列中
-let prices = [];
-
-// 當表單提交時的事件處理函數
-document.getElementById('priceForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // 防止頁面重新加載
-
-  // 獲取表單值
-  const date = document.getElementById('date').value;
-  const productName = document.getElementById('productName').value;
-  const price = document.getElementById('price').value;
-
-  // 將價格數據添加到陣列中
-  prices.push({ date, productName, price });
-
-  // 清空表單值
-  document.getElementById('date').value = '';
-  document.getElementById('productName').value = '';
-  document.getElementById('price').value = '';
-
-  // 更新表格或清單
-  updateTable();
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
-
-// 搜尋功能的事件處理函數
-function search() {
-  const searchTerm = document.getElementById('search').value;
-  const filteredPrices = prices.filter(price => price.productName.toLowerCase().includes(searchTerm.toLowerCase()));
-
-  updateTable(filteredPrices);
-}
-
-// 更新表格或清單的函數
-function updateTable(data = prices) {
-  const table = document.getElementById('priceTable');
-
-  // 清空表格內容
-  table.innerHTML = `
-    <tr>
-      <th>日期</th>
-      <th>商品名稱</th>
-      <th>商品價格</th>
-    </tr>
-  `;
-
-  // 填充表格或清單
-  data.forEach(price => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-      <td>${price.date}</td>
-      <td>${price.productName}</td>
-      <td>${price.price}</td>
-    `;
-    table.appendChild(row);
-  });
-}
---
+++ script.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    // 在頁面加載時向後端請求所有記錄
+    fetchRecords();
+
+    // 監聽表單提交事件
+    $("#recordForm").submit(function(event) {
+        event.preventDefault();
+
+        // 獲取表單數據
+        const date = $("#date").val();
+        const productName = $("#productName").val();
+        const price = $("#price").val();
+
+        // 清空輸入框
+        $("#date").val("");
+        $("#productName").val("");
+        $("#price").val("");
+
+        // 向後端發送新增記錄的請求
+        addRecord(date, productName, price);
+    });
+});
+
+// 向後端請求所有記錄
+function fetchRecords() {
+    $.ajax({
+        url: "/records",
+        type: "GET",
+        success: function(data) {
+            displayRecords(data);
+        },
+        error: function(error) {
+            console.log("Error:", error);
+        }
+    });
+}
+
+// 向後端發送新增記錄的請求
+function addRecord(date, productName, price) {
+    $.ajax({
+        url: "/records",
+        type: "POST",
+        data: {
+            date: date,
+            productName: productName,
+            price: price
+        },
+        success: function(data) {
+            displayRecord(data);
+        },
+        error: function(error) {
+            console.log("Error:", error);
+        }
+    });
+}
+
+// 將記錄顯示在前端
+function displayRecords(records) {
+    const recordsBody = $("#recordsBody");
+    recordsBody.empty();
+
+    records.forEach(function(record) {
+        const row = $("<tr></tr>");
+        const dateCell = $("<td></td>").text(record.date);
+        const productNameCell = $("<td></td>").text(record.productName);
+        const priceCell = $("<td></td>").text(record.price);
+
+        row.append(dateCell, productNameCell, priceCell);
+        recordsBody.append(row);
+    });
+}
+
+// 將新增的記錄顯示在前端
+function displayRecord(record) {
+    const row = $("<tr></tr>");
+    const dateCell = $("<td></td>").text(record.date);
+    const productNameCell = $("<td></td>").text(record.productName);
+    const priceCell = $("<td></td>").text(record.price);
+
+    row.append(dateCell, productNameCell, priceCell);
+    $("#recordsBody").append(row);
+}
